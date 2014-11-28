05ed7a8bf0843752df970f21ab6df8f9b7c06ce0
Filipa <anaborges@Anas-MacBook-Pro.local>
2014-11-28 17:04:55 +0100

    little tweak DFA

--- Biomarkers/nbt_DFA/nbt_runDFA.m
+++ Biomarkers/nbt_DFA/nbt_runDFA.m
@@ -2,7 +2,10 @@ function nbt_runDFA(Signal, SignalInfo, SaveDir,varinput)
 [hp, lp, filter_order, FitInterval, CalcInterval, DFA_Overlap, DFA_Plot, ChannelToPlot, res_logbin] = nbt_vararginHandler(varinput);
 [Signal, SignalInfo] = nbt_GetAmplitudeEnvelope(Signal, SignalInfo, hp, lp, filter_order);
 tic
-DFAObject = nbt_doDFA(Signal,SignalInfo,FitInterval, CalcInterval, DFA_Overlap, DFA_Plot, ChannelToPlot, res_logbin);
+name = genvarname (['DFA' num2str(hp) '_' num2str(lp) 'Hz']);
+%DFAObject = nbt_doDFA(Signal,SignalInfo,FitInterval, CalcInterval, DFA_Overlap, DFA_Plot, ChannelToPlot, res_logbin);
+ eval([name '= nbt_doDFA(Signal,SignalInfo,FitInterval, CalcInterval, DFA_Overlap, DFA_Plot, ChannelToPlot, res_logbin)']);%i added
 toc
-nbt_SaveClearObject('DFAObject',SignalInfo,SaveDir);
+nbt_SaveClearObject(name,SignalInfo,SaveDir);
+
 end
