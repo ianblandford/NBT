5ebaf4246649ad19e69be81423ecf11c6de1623e
simon-shlomo <poil.simonshlomo@gmail.com>
2015-02-13 00:24:41 +0100

    minor ttest2

--- Statistics/@nbt_ttest2/nbt_ttest2.m
+++ Statistics/@nbt_ttest2/nbt_ttest2.m
@@ -3,7 +3,7 @@ classdef nbt_ttest2 < nbt_UnPairedStat
     end
     
     methods
-        function obj = nbt_ttest2(obj)
+        function obj = nbt_ttest2()
             obj.testOptions.tail = 'both';
             obj.testOptions.vartype = 'equal';
             obj.testName = 'Parametric (Bi-variate): Student unpaired t-test';
@@ -13,14 +13,12 @@ classdef nbt_ttest2 < nbt_UnPairedStat
         
         function obj = calculate(obj, StudyObj)
             %Get data
-            Data1 = StudyObj.groups{obj.groups(1)}.getData(obj); %with parameters);
-            Data2 = StudyObj.groups{obj.groups(2)}.getData(obj); %with parameters);
+            Data1 = StudyObj.groups{obj.groups(1)}.getData(obj); 
+            Data2 = StudyObj.groups{obj.groups(2)}.getData(obj); 
             %Perform test
-            for bID=1:size(Data1.dataStore,1)   
+            for bID=1:Data1.numBiomarkers  
                 [~, obj.pValues(:,bID), ~, obj.statStruct{bID,1}] = ttest2(Data1{bID,1}',Data2{bID,1}','tail',  obj.testOptions.tail,'vartype', obj.testOptions.vartype);
             end
-            %options
-            
         end
     end
     
