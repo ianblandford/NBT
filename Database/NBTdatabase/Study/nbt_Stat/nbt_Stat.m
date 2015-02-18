b1df48038263a36be71b5487ef0b05be74a35da9
Simon-Shlomo Poil <poil.simonshlomo@nbt-analytics.com>
2015-02-18 17:42:45 +0100

    moved getBiomarkerNames up to nbt_Analysis

--- Database/NBTdatabase/Study/nbt_Stat/nbt_Stat.m
+++ Database/NBTdatabase/Study/nbt_Stat/nbt_Stat.m
@@ -21,26 +21,7 @@ classdef (Abstract) nbt_Stat < nbt_Analysis
         function obj = calculate(obj)
         end
         % checkPreCondition();
-        
-        function biomarkerNames = getBiomarkerNames(StatObj)
-            for m=1:length(StatObj.group{1}.biomarkers)
-                prefixIdx = strfind(StatObj.group{1}.biomarkers{m},'_');
-                prefixIdx = prefixIdx(end);
-                identFlag = true;
-                for identLoop = 1:size(StatObj.group{1}.biomarkerIdentifiers{m},1)
-                    if(strcmp(StatObj.group{1}.biomarkerIdentifiers{m}{identLoop},'frequencyRange'))
-                        biomarkerNames{m} = [StatObj.group{1}.biomarkers{m}(prefixIdx+1:end) '.' StatObj.group{1}.subBiomarkers{m} ' : ' StatObj.group{1}.biomarkerIdentifiers{m}{identLoop,2}];
-                        identFlag = false;
-                    end
-                end
-                if(identFlag)
-                    biomarkerNames{m} = [StatObj.group{1}.biomarkers{m}(prefixIdx+1:end) '.' StatObj.group{1}.subBiomarkers{m}];
-                end
-                biomarkerNames{m} = strrep(biomarkerNames{m},'_','.');
-            end
-            
-        end
-        
+                
     end
     
 end
