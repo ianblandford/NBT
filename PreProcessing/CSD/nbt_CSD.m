95d2fcc32fac3df186de3d30b4843ec30befe350
Simon Houtman <simon.houtman@gmail.com>
2015-03-12 18:19:39 +0100

    CSD Signal and SignalInfo are saved, closes #87

--- PreProcessing/CSD/nbt_CSD.m
+++ PreProcessing/CSD/nbt_CSD.m
@@ -1,4 +1,57 @@
-function [Signal, SignalInfo] = nbt_CSD(Signal,SignalInfo,Save_dir)
+% nbt_CSD(Signal,SignalInfo,SaveDir) calculates the scalp current source
+% density for a given signal based on the spherical spline surface
+% Laplacian as suggested by Perrin et al. (1989, 1990). Wrapper around
+% computeCSD.m of which the contents are the same as CSD.m from the CSD
+% Toolbox (Copyright (C) 2003 by Jürgen Kayser)
+%
+% Usage:
+%   nbt_CSD(Signal,SignalInfo,SaveDir)
+%
+% Inputs:
+%   Signal = A raw EEG signal imported in NBT format
+%   SignalInfo = The SignalInfo corresponding to the given Signal
+%   SaveDir = The folder where the Signal and SignalInfo files are saved
+
+% Outputs:
+%   CSDSignal = Current Source Density (CSD) transformed EEG data
+%   CSDSignalInfo = The CSD SignalInfo corresponding to the CSDSignal
+%
+% Example:
+%   nbt_CSD(Signal,SignalInfo,SaveDir)
+%   
+%
+% References:
+%
+% See also:
+%   computeCSD.m
+
+%------------------------------------------------------------------------------------
+% Originally created by Simon J. Houtman (2015), see NBT website for current email address
+%------------------------------------------------------------------------------------
+%
+% ChangeLog - see version control log at NBT website for details.
+%
+%
+% Part of the Neurophysiological Biomarker Toolbox (NBT)
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% See Readme.txt for additional copyright information.
+% ---------------------------------------------------------------------------------------
+
+function [CSDSignal, CSDSignalInfo] = nbt_CSD(Signal,SignalInfo,SaveDir)
     %%% Get the channel information
     chanLocs = SignalInfo.interface.EEG.chanlocs;
     
@@ -13,29 +66,32 @@ function [Signal, SignalInfo] = nbt_CSD(Signal,SignalInfo,Save_dir)
         chanInfo.phi(chan) = chanLocs(chan).sph_phi;
     end
     
+    %%% Spline flexibility
+    splineFlexibility = 4;
+    
     tic
     %%% Compute G and H using CSD toolbox 'GetGH.m'
-    [G,H] = GetGH(chanInfo);
+    [G,H] = GetGH(chanInfo,splineFlexibility);
     
     %%% Define CSDSignal
     nTimePoints = size(Signal,1);
     CSDSignal = zeros(nTimePoints,size(Signal,2));
 
     %%% Compute CSD using CSD toolbox 'CSD.m'
-    text = ['Computing scalp current density for ', num2str(nTimePoints), ' time points'];
-    disp(text);
+    disp(['Computing scalp current source density for ', num2str(nTimePoints), ' time points']);
     for timePoint = 1 : size(Signal,1)
-        if mod((timePoint / nTimePoints * 100),1) == 0
-            disp([num2str(timePoint / nTimePoints * 100), '%']);
-        end
         CSDSignal(timePoint,:) = computeCSD(Signal(timePoint,:)',G,H);
     end
     toc
     
-    %%% Set the Signal and SignalInfo
-    Signal = CSDSignal;
-    
     %%% Set the SignalInfo
-    SignalInfo.signalName = 'CSDSignal';
-    SignalInfo.signalType = 'CSDSignal';
+    CSDSignalInfo = SignalInfo;
+    CSDSignalInfo.signalName = 'CSDSignal';
+    CSDSignalInfo.signalType = 'CSDSignal';
+    CSDSignalInfo.filterSettings = struct('splineFlexibility',splineFlexibility);
+    
+    %%% Save the Signal and SignalInfo
+    disp('Appending to existing signal file.');
+    save(strcat([SaveDir '\' SignalInfo.subjectInfo '.mat']),'CSDSignal','-append');
+    save(strcat([SaveDir '\' SignalInfo.subjectInfo '_info.mat']),'CSDSignalInfo','-append');
 end
